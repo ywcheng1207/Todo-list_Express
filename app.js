e031fa68b93b85decefda817a19709306cb469db
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-08-03 17:49:05 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -94,6 +94,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .then(() => res.redirect(`/todos/${id}`))
     .catch((error) => console.log(error))
 })
+// Delete
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then((todo) => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
 //--------------------------------------------------------------------------------------
 //----------------------------------- 啟動  ---------------------------------------------
 //--------------------------------------------------------------------------------------
