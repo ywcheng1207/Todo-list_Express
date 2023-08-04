678d87a762512ab818b12f29e6a111ff14fcba2d
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-08-04 23:55:56 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -4,45 +4,20 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes') // 對Express來說，等同./reoutes/index.js
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-// --------------------------------------------------------------------------------------
-// ------------------------------------- db區 -------------------------------------------
-// --------------------------------------------------------------------------------------
-const app = express()
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 // --------------------------------------------------------------------------------------
 // ---------------------------------- use區 ---------------------------------------------
 // --------------------------------------------------------------------------------------
-
+const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
 // --------------------------------------------------------------------------------------
 // ----------------------------------- 啟動  ---------------------------------------------
 // --------------------------------------------------------------------------------------
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,21 +1,8 @@
-const mongoose = require('mongoose')
-const Todo = require('../todo') // 載入 todo model
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const Todo = require('../todo')
+const db = require('../../config/mongoose')
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` })
+    Todo.create({ name: 'name-' + i })
   }
   console.log('done')
 })
