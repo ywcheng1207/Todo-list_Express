6f8ac59f8617bcaed03f14cc8eb18ca3e24be85b
ywcheng1207 <ywcheng1207.git@gmail.com>
2024-04-20 14:22:19 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // --------------------------------------------------------------------------------------
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -16,6 +17,11 @@ const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
