b4e4371514315b7544c1971210df200e9bc30f21
YWJ <ywcheng1207.git@gmail.com>
2024-05-14 16:27:35 +0800

    feat: add relations between User and Todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,42 +5,47 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const name = req.body.name
-  return Todo.create({ name })
+  return Todo.create({ name, userId })
     .then(() => res.redirect('/'))
-    .catch((error) => console.log(error))
+    .catch(error => console.log(error))
 })
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch((error) => console.log(error))
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
 })
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch((error) => console.log(error))
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
 })
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then((todo) => {
+  return Todo.findOne({ _id, userId })
+    .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch((error) => console.log(error))
+    .then(() => res.redirect(`/todos/${_id}`))
+    .catch(error => console.log(error))
 })
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then((todo) => todo.remove())
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
+    .then(todo => todo.remove())
     .then(() => res.redirect('/'))
-    .catch((error) => console.log(error))
+    .catch(error => console.log(error))
 })
 module.exports = router
