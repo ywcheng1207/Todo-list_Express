a468bbbe6ab16c75d43c6295b5eb649e28f2024c
YWJ <ywcheng1207.git@gmail.com>
2024-05-14 16:06:18 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const session = require('express-session')
+const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -24,6 +25,14 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+app.use((req, res, next) => {
+  console.log('【登入成功】',req.user) 
+  // res.locals 是 Express.js 幫我們開的一條捷徑，放在 res.locals 裡的資料，所有的 view 都可以存取
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 // --------------------------------------------------------------------------------------
 // ----------------------------------- 啟動  ---------------------------------------------
